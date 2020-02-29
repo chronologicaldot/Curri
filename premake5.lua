5afd6d5f1fce0c4a8bbfff48c695fa2c1f546df5
chronologicaldot <anderocketech@gmail.com>
2020-02-28 18:42:32 -0600

    Last update

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,17 @@ local v_b_copper_path = "../" .. v_copper_path
 local v_b_copper_stdlib_path = "../" .. v_copper_stdlib_path
 --local v_b_irrext_path = "../" .. v_irrext_path
 
+-- excluded files
+local nix_files = { "src/standalone/App.h", "src/standalone/App.cpp" }
+
+
+local standalone = false
+
+if (standalone)
+then
+	nix_files = { "src/App.h", "src/App.cpp" }
+end
+
 
 workspace "Curri App"
 	configurations	{ "debug", "release" }
@@ -65,6 +76,7 @@ project "Curri App"
 		, "src/excludes/**.cpp"
 		, v_cubr_path .. "excludes/**.h"
 		, v_cubr_path .. "excludes/**.cpp"
+		, nix_files
 	}
 	buildoptions {
 		"-I" .. v_b_cubr_path
